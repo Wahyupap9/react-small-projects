4ab414147b75c9d6ff9467d037b1761330e15497
Wahyupap9 <wahyupap9@gmail.com>
2024-12-25 16:07:42 +0800

    done making random color generator (also adding feature to change rgb => hex and hex => rgb)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Accordian from "./components/Accordian/Accordian";
+import RandomColorGenerator from "./components/Random-color-generator/randomColorGenerator";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Accordian />
+    <RandomColorGenerator />
   </StrictMode>
 );
